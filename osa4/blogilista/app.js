3c5de9ccc41b2980fd08f99bb221d4d204ecb198
Matti <matti.aalto@tuni.fi>
2022-01-15 20:19:54 +0200

    Blogilista muokattu tehtävän 4.1 mukaiseen tilaan.

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -12,8 +13,7 @@ const blogSchema = mongoose.Schema({
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
