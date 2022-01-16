0a509337b8eeef7a14a648ad5a941836c58989ba
Matti <matti.aalto@tuni.fi>
2022-01-16 18:00:27 +0200

    Linttaus lisätty ja toimiva versio kohdasta 4.3

---
+++ osa4/blogilista/.eslintrc.js
@@ -0,0 +1,51 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent*/
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true,
+		'node': true,
+		'jest': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 13
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			4
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0,
+	}
+}
