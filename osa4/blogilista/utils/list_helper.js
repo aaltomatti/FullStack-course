03e410edb71ae83ff892f10cf1354aec4ac21df7
Matti <matti.aalto@tuni.fi>
2022-01-16 20:39:31 +0200

    favoriteBlog funktion testaus ja toiminta implementoitu. Tehtävä 4.5 tehty

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,10 +2,25 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (array) => {
-    return array.reduce((sum, item) => sum + item.likes, 0 )
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0 )
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (favorite, blog) => {
+        if (blog.likes > favorite.likes){
+            favorite = {
+                title: blog.title,
+                author: blog.author,
+                likes: blog.likes
+            }
+        }
+        return favorite
+    }
+    return blogs.length === 0
+    ? 'No blogs'
+    : blogs.reduce(reducer, { title: blogs[0].title, author: blogs[0].author, likes: blogs[0].likes })
+}
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
